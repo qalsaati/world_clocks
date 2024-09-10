d3f9fef0b7d7ee1c1d77b3f64854529d310c9cbb
terencegaffudSU <99375248+terencegaffudSU@users.noreply.github.com>
2024-09-10 10:13:42 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import logo from "/logo.png";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div>
-        <img src={logo} className="logo" alt="Rocket logo" />
-      </div>
       <h1>World Clock</h1>
       <div className="card">
         <p>
